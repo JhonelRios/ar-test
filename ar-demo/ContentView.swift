f4297aad118e5521430ee2c65a59dbb7ec8991d3
jhonelrios <riosjaimes.j@gmail.com>
2024-04-04 22:40:17 -0500

    feat: horizontal plane anchor

--- ar-demo/ContentView.swift
+++ ar-demo/ContentView.swift
@@ -7,22 +7,50 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    @State private var modelRotation: Float = 0
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack {
+            ARViewContainer(modelRotation: $modelRotation).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                Button(action: {
+                    self.modelRotation += .pi/4
+                }, label: {
+                    Text("Rotar")
+                        .padding()
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                })
+                .padding(.bottom, 20)
+            }
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    @Binding var modelRotation: Float
     
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
         
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        
+        arView.debugOptions = .showAnchorGeometry
+        arView.session.run(config)
+        
         let llamaEntity = try! Entity.load(named: "llama")
-        llamaEntity.scale = SIMD3(0.5, 0.5, 0.5)
-        llamaEntity.transform.translation = [0, -2.5, -8]
+        llamaEntity.scale = SIMD3(0.1, 0.1, 0.1)
+        llamaEntity.transform.translation.y = 0.05
+        context.coordinator.llamaEntity = llamaEntity
+//        llamaEntity.transform.translation = [0, -2.5, -8]
         
         // Create a cube model
 //        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
@@ -33,7 +61,7 @@ struct ARViewContainer: UIViewRepresentable {
 //        model.transform.translation = [0, 0, -0.5]
         
         // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(world: .zero)
+        let anchor = AnchorEntity(plane: .horizontal, classification: .any, minimumBounds: [0.2, 0.2])
         anchor.children.append(llamaEntity)
         
         // Add the horizontal plane anchor to the scene
@@ -47,7 +75,9 @@ struct ARViewContainer: UIViewRepresentable {
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        context.coordinator.rotateModel(modelRotation: modelRotation)
+    }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -55,9 +85,14 @@ struct ARViewContainer: UIViewRepresentable {
     
     class Coordinator : NSObject {
         var parent: ARViewContainer
+        var llamaEntity: Entity?
         
         init(_ parent: ARViewContainer) {
             self.parent = parent
+        }
+        
+        func rotateModel(modelRotation: Float) {
+            llamaEntity?.transform.rotation = simd_quatf(angle: modelRotation, axis: [0, 1, 0])
         }
         
         @objc func handleTap(_ sender: UITapGestureRecognizer) {
